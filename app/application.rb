2bd9ac2af7739421dea870fec2b825a7afc80c60
Brendan O'Dea <brendan.ode4@gmail.com>
2018-03-20 02:57:34 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -4,6 +4,16 @@ class Application
 
   if req.path.match(/items/)
     m = Item.all.select(|i| i.name == req.split("/items/").last)
-
+    if m.empty?
+      resp.write "Item not found"
+      resp.status  = 400
+    else
+      resp.write "#{m.first.price}"
+      resp.status = 200
+    end
+  else
+    resp.write "Route not found"
+    resp.status = 404
+  end
 
 end
